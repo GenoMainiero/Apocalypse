e6c054eec27601207de96233222ef826a9d4174f
Kenny <tico121@live.com>
2018-10-12 14:48:38 -0400

    Final code for help command

--- Game/Game/Main.cpp
+++ Game/Game/Main.cpp
@@ -5,9 +5,13 @@
 
 using namespace std;
 
+
+
+
+
 int main()
 {
-	//use string for help command
+	//input string from user
 	string help;
 	
 	//video game name
@@ -18,29 +22,52 @@ int main()
 	
 	//video game storyline
 	cout << " You are currently in a large city following a series of natural disasters, you are believed to be the only living person within this city." << endl;
-	cout << " There is also reason to believe that others may have also survided in other cities " << endl;
+	cout << " There is also reason to believe that others may have also survided in other cities. " << endl;
 	cout << " A nearby radio tower can be used to communicate with other survivors, your mission is to find this radio tower and communicate with the other surviors." << endl;
 	cout << " However, you will also need to find food and water. " << endl;
 
 	//creates a white space
 	cout << " \n\n ";
 	
-	//video game list of commands shows at start
-	cout << " The following are a list of commands to help you navigate through the game " << endl;  
+	//video game lists help commands
+	cout << " The following are a list of commands to help you navigate through the game: " << endl;  
 
+	//white space
+	cout << " \n\ ";
+	
 	// How to use Directions
-	cout << " For Cardinal Directions, use the following commands: North, East, South, West " << endl;
+	cout << " For Movement, use the following commands: North, East, South, West, Jump. " << endl;
 
 	// Actions
-	cout << " For Actions, use the following commands: Throw, Jump, Look, Open, Close, Take, Drop, Eat, Drink " << endl;
+	cout << " For Actions, use the following commands: Search, Open, Close, Grab, Drop, Use. " << endl;
 
-	//Program notifies user of help command option
-	cout << " If you need help at any time, please type in help " << endl;
-	
-	// user enters help
-	cin >> help;
-	
-	// returns all of above input/output
+	cout << "\n ";
+
+//Program notifies user of help command option
+	cout << " If you need help, please type in help. " << endl; 
+
+	getline(cin, help);
+
+	//if else statement makes sure the right word "help" is being typed. 
+	if (help == "help")
+	{
+		cout << " For Movement, use the following commands: North, East, South, West, Jump. " << endl;
+		cout << " For Actions, use the following commands: Search, Open, Close, Grab, Drop, Use. " << endl;
+	}
+	else
+	{// while repeats itself until the right word "help" is typed, then it exits.
+		while (help != "help")
+		{
+			cout << " Invalid Input, please type help for the help command " << endl;
+			getline(cin, help);
+		}
+		//once help is typed then show help command again.
+		cout << " For Movement, use the following commands: North, East, South, West, Jump. " << endl;
+		cout << " For Actions, use the following commands: Search, Open, Close, Grab, Drop, Use. " << endl;
+	}
+
+	//other code follows to continue playing the game
+
 	system("pause");
 	return 0;
 

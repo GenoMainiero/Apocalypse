a50853a8d8abf394e7e4cd01dc0ea4d120008544
Geno <genomainiero@gmail.com>
2018-10-31 13:44:48 -0400

    Apocalypse 2.0.2 added, this is now the correct code. errors merging

---
+++ Apocalypse 2.0.2/Apocalypse/Inventory.h
@@ -0,0 +1,51 @@
+#pragma once
+#ifndef INVENTORY_H
+#define INVENTORY_H
+#include "ItemsArray.h"
+#include <list>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Inventory
+{
+private:
+	vector<Item*> itemVector;
+public:
+	bool itworked = 0;
+	vector<Item*> getInventory()
+	{
+		return itemVector;
+	}
+	Item * getItem(int index)
+	{
+		return itemVector[index];
+	}
+	int getSize()
+	{
+		return itemVector.size();
+	}
+	Item * getBack()
+	{
+		return itemVector.back();
+	}
+	void addItem(Item *item)
+	{
+		itemVector.push_back(item);
+		item->setPossess(1);
+	}
+	void removeItem(int index)
+	{
+		for (int i = 0; i < itemVector.size(); i++)
+		{
+			if (itemVector[i]->getName() == itemVector[index]->getName())
+			{
+				itemVector.erase(itemVector.begin() + i);
+				itworked = 1;
+			}
+		}
+		if (!itworked)
+			cout << "This item could not be found in your inventory.";
+	}
+};
+#endif
